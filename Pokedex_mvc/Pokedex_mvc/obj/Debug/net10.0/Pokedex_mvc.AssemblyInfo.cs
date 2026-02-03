77a5927edb4f29c9f285a4306081ad231671c1ab
unknown <lucas_mineo@outlook.com>
2026-02-03 19:23:14 -0300

    actividad final unidad 2

--- Pokedex_mvc/Pokedex_mvc/obj/Debug/net10.0/Pokedex_mvc.AssemblyInfo.cs
+++ Pokedex_mvc/Pokedex_mvc/obj/Debug/net10.0/Pokedex_mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex_mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8b831e3fe6246ee85c79cd1f6dc9fd1de4521bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4029b999f50d3755b19a6909633854859ded650e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex_mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex_mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
